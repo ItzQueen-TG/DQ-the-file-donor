ce9cd6e8c16da91f136aaad5fb52ccb4850faef5
Mуѕтєяισυѕ gιяℓ <102360292+ItzQueen-TG@users.noreply.github.com>
2023-06-08 17:32:19 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -653,14 +653,9 @@ async def send_all(bot, userid, files, ident):
                 caption=f_caption,
                 protect_content=True if ident == "filep" else False,
                 reply_markup=InlineKeyboardMarkup(
-                    [
-                        [
-                        InlineKeyboardButton('Sᴜᴘᴘᴏʀᴛ Gʀᴏᴜᴘ', url=GRP_LNK),
-                        InlineKeyboardButton('Uᴘᴅᴀᴛᴇs Cʜᴀɴɴᴇʟ', url=CHNL_LNK)
-                    ],[
-                        InlineKeyboardButton("Bᴏᴛ Oᴡɴᴇʀ", url="t.me/creatorbeatz")
-                        ]
-                    ]
+                    [[
+                      InlineKeyboardButton('Uᴘᴅᴀᴛᴇ ♥️', url="https://t.me/Movies_adda24_7")
+                    ]]
                 )
             )
         except UserIsBlocked:
